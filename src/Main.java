dbbbcc7ba0b4d37cbee5854a57b16a160e0846cc
SCKboss <=>
2023-12-05 13:17:38 +0700

    lab05

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
-
-
-
 public class Main {
-        public static void main(String[] args) {
-            Character SCK = new Warrior("SCKagura", "Warrior") ;
-            Sword Sword = new Sword() ;
-            SCK.showStats(); 
-            Daggers daggers = new Daggers() ;
-            SCK.EquipWeapon(daggers);
-            SCK.EquipWeapon(Sword);
-            SCK.showStats();
-        Character DUMMY = new Warrior("Dummy", "Warrior") ;
+    public static void main(String[] args) {
+        Character SCK = new Warrior("SCKagura", "Warrior");
+        Sword sword = new Sword();
+        SCK.showStats();
+        Daggers daggers = new Daggers();
+        SCK.EquipWeapon(daggers);
+        SCK.EquipWeapon(sword);
+        SCK.showStats();
 
-        SCK.attack(DUMMY) ;
-        DUMMY.showStats(); 
-        }
+        Character DUMMY = new Warrior("Dummy", "Warrior");
 
+        SCK.attack(DUMMY);
+        DUMMY.showStats();
+
+
+        Character assassin = new Assasin("Assassin1", "Assassin");
+        Sword assassinSword = new Sword();
+        assassin.EquipWeapon(assassinSword);
+        assassin.showStats();
+
+        Character assassinTarget = new Assasin("AssassinTarget", "Assassin");
+        assassin.attack(assassinTarget);
+        assassinTarget.showStats();
+    }
 }
